9e2f974d515b8c79613ad95d90907d3925bffb61
shiladitya357 <shiladitya357@gmail.com>
2020-07-05 23:57:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+E-commerce website using NodeJS, ExpressJS, MongoDB, Mongoose, Stripe, Elasticsearch
---
+++ config/secret.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+  database: 'mongodb+srv://root:shila123@ecomm1-wgj8f.mongodb.net/test?retryWrites=true&w=majority',
+  port: 3000,
+  secretKey: "Shila123"
+}
