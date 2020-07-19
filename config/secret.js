3496e9f2fffb05b7460a8220dd15e40cbd3d90fd
Shiladitya Gangopadhyay <shiladitya357@gmail.com>
2020-07-19 21:01:52 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  database: 'mongodb+srv://root:shila123@ecomm1-wgj8f.mongodb.net/test?retryWrites=true&w=majority',
+  database: 'mongodb+srv://mongodbatlas_username:mongodbatlas_password@ecomm1-wgj8f.mongodb.net/test?retryWrites=true&w=majority',
   port: 3000,
-  secretKey: "Shila123"
+  secretKey: "<secret key>"
 }
